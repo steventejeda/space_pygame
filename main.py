010275b1238a78a370e5d932d53ef131bbbe7165
steventejeda <steventejeda@gmail.com>
2021-08-25 21:48:46 -0400

    completed game

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
 BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100 )
 
 FPS = 60
 VELOCITY = 5
@@ -33,8 +35,16 @@ SPACE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'space.p
 def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
     WIN.blit(SPACE, (0,0))
     pygame.draw.rect(WIN, BLACK, BORDER)
+    
+    red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, WHITE)
+    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
+    WIN.blit(yellow_health_text, (10,10))
+    
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
+    
+    
     
     for bullet in red_bullets: 
         pygame.draw.rect(WIN, RED, bullet)
@@ -80,7 +90,13 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
             red_bullets.remove(bullet)
         elif bullet.x < 0:
             red_bullets.remove(bullet)
-
+            
+def draw_winner(text): 
+    draw_text = WINNER_FONT.render(text, 1, WHITE)
+    WIN.blit(draw_text, (WIDTH / 2 - draw_text.get_width() / 2, HEIGHT / 2 - draw_text.get_height() / 2 ))
+    pygame.display.update()
+    pygame.time.delay(5000)
+    
 
 def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -125,7 +141,8 @@ def main():
             winner_text = "Red wins!"
         
         if winner_text != "":
-            pass #Someone won        
+            draw_winner(winner_text)
+            break     
         
         keys_pressed = pygame.key.get_pressed()
         
